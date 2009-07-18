d642987fc1aa89f1af370d460c8218e37e648f42
Christian Hofstaedtler <christian@hofstaedtler.name>
2009-07-19 01:00:50 +0200

    support mediawiki > 1.11.x

--- wiki-exportimport/src/sicekit_wiki_exportimport/exporter.py
+++ wiki-exportimport/src/sicekit_wiki_exportimport/exporter.py
@@ -1,41 +1,51 @@
+# vim:set et!:
+
 from wikitools import api
 from optparse import OptionParser
 import os
+import re
 
 class Exporter(object):
 	def __init__(self, configuration, wiki):
 		self.configuration = configuration
 		self.wiki = wiki
+		self.wikiversion = re.search("\d\.(\d\d)", self.wiki.siteinfo['generator'])
+
+	def _buildCMListQuery(self, categoryname):
+		params = {'action':'query', 'list':'categorymembers', 'cmprop':'ids|title|timestamp'}
+		if not int(self.wikiversion.group(1)) >= 13:
+			params['cmcategory'] = categoryname
+		else:
+			params['cmtitle'] = 'Category:%s' % categoryname
+		return params
 
 	def generatePageList(self):
-
-		# define the params for the query
-		params = {'action':'query', 'list':'categorymembers', 'cmcategory':'Origin-SICEKIT', 'cmprop':'ids|title|timestamp'}
-		# create the request object
+		params = self._buildCMListQuery('Origin-SICEKIT')
 		request = api.APIRequest(self.wiki, params)
-		# query the API
 		return request.query()['query']['categorymembers']
 
 	def _buildDirFilename(self, page):
 		(directory, filename) = page[u'title'].replace(':','/').rsplit('/', 1)
 		return (os.path.join(self.options.export_path, directory), filename+'.xml')
 
-	def exportPage(self, page):
-		title = page[u'title']
-		print " === Exporting page %s ===" % title
-
-		params = {'action':'query', 'titles':title,'export':'1'}
-		# create the request object
-		request = api.APIRequest(self.wiki, params)
-		# query the API
-		xmldump = request.query()['query']['export']['*']
-
+	def writeDumpFile(self, page, xmldump):
 		(directory, filename) = self._buildDirFilename(page)
 		if not os.path.exists(directory): os.makedirs(directory)
 
 		f = file(os.path.join(directory, filename), 'w')
 		f.write(xmldump)
 		f.close()
+
+	def exportPage(self, page):
+		title = page[u'title']
+		print " === Exporting page %s ===" % title
+
+		params = {'action':'query', 'titles':title,'export':'1'}
+		request = api.APIRequest(self.wiki, params)
+		xmldump = request.query()['query']['export']['*']
+
+		self.writeDumpFile(page, xmldump)
+
 		self.pagecount = self.pagecount + 1
 		self.bytecount = self.bytecount + len(xmldump)
 
@@ -43,7 +53,6 @@ class Exporter(object):
 		self.pagecount = 0
 		self.bytecount = 0
 		map(self.exportPage, pages)
-
 
 	def run(self, argv):
 		op = OptionParser()
